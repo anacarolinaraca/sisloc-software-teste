e6fb61c1a4efc5f6844cfe3b3372691a703f09e5
Ana Carolina Rodrigues <ana.rodrigues@gosat.org>
2024-10-29 19:40:46 -0300

    Ajuste de texto

--- src/utils/calculateTicketPrice.ts
+++ src/utils/calculateTicketPrice.ts
@@ -5,11 +5,13 @@ export default function calculateTicketPrice(quantity: number): number {
   if (quantity <= 5) {
     sumTicket = quantity * 50;
   } else if (quantity >= 6 && quantity <= 15) {
-    sumTicket = (5 * 50) + ((quantity - 5) * 45);
-  }  else {
-    sumTicket = (5 * 50) + (10 * 45) + ((quantity - 15) * 40);
+    sumTicket = 5 * 50 + (quantity - 5) * 45;
+  } else {
+    sumTicket = 5 * 50 + 10 * 45 + (quantity - 15) * 40;
   }
   return sumTicket;
 }
 
-// Foi utilizada a estrutura condicional, por ser de fácil manutenção. Também optei organizar a função na pasta utils, pois facilita sua reutilização em outras partes do projeto.
+/* Foi utilizada a estrutura condicional, por ser de fácil manutenção. Também optei organizar esta função na pasta utils, pois facilita sua reutilização em outros locais do projeto. 
+A organização das pastas segue um padrão estabelecido em projetos realizados no curso da trybe.
+*/
