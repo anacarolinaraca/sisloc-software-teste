db8a5f5731624421a52c6b26ff35cf9278bd99f1
Ana Carolina Rodrigues <ana.rodrigues@gosat.org>
2024-10-31 01:33:54 -0300

    Configuração para que o express receba e processe requisições em json

--- src/app.ts
+++ src/app.ts
@@ -2,21 +2,22 @@ import express, { Application } from 'express';
 import routes from './routes/ticket.routes';
 
 class App {
-  public app: Application;
-  constructor() {
-      this.app = express();
-      this.routes();
-  }
-  
-  private routes():void {
-      this.app.use('/api', routes);
-  }
+    public app: Application;
+    constructor() {
+        this.app = express();
+        this.app.use(express.json());
+        this.routes();
+    }
 
-  public start(PORT: number): void {
-      this.app.listen(PORT, () => {
-          console.log(`Server running on port ${PORT}`);
-      });
-  }
+    private routes(): void {
+        this.app.use('/api', routes);
+    }
+
+    public start(PORT: number): void {
+        this.app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+        });
+    }
 }
 
 export default App;
