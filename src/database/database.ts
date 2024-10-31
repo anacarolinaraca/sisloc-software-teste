8ce0f625e3ef4cf800c1a15afc83f9fdd41d8a28
Ana Carolina Rodrigues <ana.rodrigues@gosat.org>
2024-10-31 09:59:47 -0300

    ajuste conexão com banco de dados

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,20 +1,29 @@
 import mysql, { Pool } from 'mysql2/promise';
 
-/* Configuração com o banco de dados */
-const dbConfig = {
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  port: Number(process.env.DB_PORT) || 3306,
-  password: process.env.DB_PASSWORD || 'password',
-  database: process.env.DB_NAME || 'tickets_db',
-};
 
 /* Cria um pool de conexões com o banco de dados e verifica se o banco de dados existe, caso contrário, cria o banco. */
 export async function createConnection() {
-  const connection: Pool = mysql.createPool(dbConfig);
+  const connection = await mysql.createConnection({
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    port: Number(process.env.DB_PORT) || 3306,
+    password: process.env.DB_PASSWORD || 'password',
+  });
+
   await connection.query(`CREATE DATABASE IF NOT EXISTS tickets_db`);
-  await connection.getConnection();
-  return connection;
+
+  await connection.end();
+
+  /* Configuração com o banco de dados */
+  const dbConfig = {
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    port: Number(process.env.DB_PORT) || 3306,
+    password: process.env.DB_PASSWORD || 'password',
+    database: process.env.DB_NAME || 'tickets_db',
+  };
+  const connectionPool: Pool = mysql.createPool(dbConfig);
+  return connectionPool;
 }
 
 /* Query base para executar os comando SQL */
